90e0fbde1d87a0bdec6d7dfd614229f7b6bb01bc
RadAd <adamgates84+github@gmail.com>
2025-07-31 17:11:31 +1000

    Remove sync

--- sync_radlibplus.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set DEST=%1
-if not defined DEST set DEST=Rad
-xcopy /D /U %DEST% "%USERPROFILE%\Sync\Utils\RadLibPlus\Win\"
-xcopy /D /U "%USERPROFILE%\Sync\Utils\RadLibPlus\Win\" %DEST%
